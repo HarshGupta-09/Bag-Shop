48df8b16153040c29f661889dbbfef6e6df5a0bd
Harsh Gupta <harshgupta82003@gmail.com>
2026-03-03 17:49:35 +0530

    Create admin

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const router = express.Router();
 const isLoggedIn = require("../middleware/isLoggedIn")
-
+const productModel = require("../models/productModel")
 router.get("/",(req,res)=>{
    let error = req.flash("error")
    res.render("index",{ error })
 })
-router.get("/shop",isLoggedIn,(req,res)=>{
- res.render("shop")
-})
-
+router.get("/shop", isLoggedIn, async (req, res) => {
+   const products = await productModel.find();
+   res.render("shop", { products });
+});
 module.exports = router;
--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -3,39 +3,20 @@ const router = express.Router();
 const ownerModel= require("../models/ownerModel")
 const bcrypt = require("bcrypt")
 
-router.get("/",(req,res)=>{
-    res.send("welcome to owners route")
+router.get("/login",(req,res)=>{
+    res.render("ownerLogin")
+    // to do
 })
 
-router.post("/create", async (req, res) => {
-   try {
-   console.log(req.body)
-      const { email, fullname , password } = req.body;
-console.log(fullname)
-      const existingOwner = await ownerModel.findOne({ email });
 
-      if (existingOwner) {
-         return res.status(400).json({
-            message: "User with this email already exists"
-         });
-      }
-      const hashPassword = await  bcrypt.hash(password,10)
+router.post("/login" , (req,res)=>{
+// to do
+})
 
-      const newOwner = await ownerModel.create({
-       ...req.body,
-        password : hashPassword
-      });
+router.get("/owner/admin/dash",(req,res)=>{
+   res.send("Admin Dashboard")
+})
 
-      res.status(201).json({
-         message: "Owner created successfully",
-         newOwner
-      });
 
-   } catch (error) {
-      res.status(500).json({
-         message: "Internal Server Error"
-      });
-   }
-});
 
 module.exports = router;
